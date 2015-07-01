dd4c4962103f2c8f0ea7e152b7d437fab459e7c4
Henry Phan <hphan.cs@gmail.com>
2015-06-30 21:20:17 -0400

    Added route to login page/homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "devise/sessions#new"
+
+	devise_scope :user do
+	  authenticated :user do
+	    root 'home#index', as: :authenticated_root
+	  end
+
+	  unauthenticated do
+	    root 'devise/sessions#new', as: :unauthenticated_root
+	  end
+	end
+
 end
